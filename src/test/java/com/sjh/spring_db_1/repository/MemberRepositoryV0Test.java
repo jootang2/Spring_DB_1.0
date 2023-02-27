ac683cdd3036b9b2650e0871e5c0173018def7cc
joohwan9607 <joohwan9607@mobility42.io>
2023-02-28 01:03:56 +0900

    JDBC find

--- src/test/java/com/sjh/spring_db_1/repository/MemberRepositoryV0Test.java
+++ src/test/java/com/sjh/spring_db_1/repository/MemberRepositoryV0Test.java
@@ -1,19 +1,28 @@
 package com.sjh.spring_db_1.repository;
 
 import com.sjh.spring_db_1.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
-
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+        //save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
