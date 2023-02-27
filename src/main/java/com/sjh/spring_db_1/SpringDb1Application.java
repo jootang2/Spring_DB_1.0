7057d30fc53857f22796a458f4919e1c5a718759
joohwan <joohwan9607@mobility42.io>
2023-02-27 19:19:26 +0900

    connect h2 DB

--- src/main/java/com/sjh/spring_db_1/SpringDb1Application.java
+++ src/main/java/com/sjh/spring_db_1/SpringDb1Application.java
@@ -2,8 +2,10 @@ package com.sjh.spring_db_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class SpringDb1Application {
 
     public static void main(String[] args) {
