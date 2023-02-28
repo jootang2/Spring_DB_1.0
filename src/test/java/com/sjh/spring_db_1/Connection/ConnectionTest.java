87a8cb526185110138bce82dbd4ec29661190c1f
joohwan9607 <joohwan9607@mobility42.io>
2023-03-01 02:11:51 +0900

    example DataSource - DriverSource, JdbcUtils

--- src/test/java/com/sjh/spring_db_1/Connection/ConnectionTest.java
+++ src/test/java/com/sjh/spring_db_1/Connection/ConnectionTest.java
@@ -1,6 +1,7 @@
 package com.sjh.spring_db_1.Connection;
 
 import com.sjh.spring_db_1.connection.ConnectionConst;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -21,6 +22,8 @@ public class ConnectionTest {
         Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         log.info("connection = {}, class = {}", con1, con1.getClass());
         log.info("connection = {}, class = {}", con2, con2.getClass());
+        con1.close();
+        con2.close();
     }
 
     @Test
@@ -30,10 +33,26 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(20);
+        dataSource.setPoolName("Juhwan Pool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
     private void useDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection();
         Connection con2 = dataSource.getConnection();
         log.info("connection = {}, class = {}", con1, con1.getClass());
         log.info("connection = {}, class = {}", con2, con2.getClass());
+        con1.close();
+        con2.close();
     }
 }
